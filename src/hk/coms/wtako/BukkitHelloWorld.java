ce65fcf46797dec9667d54ccca2f31f8a1f6e473
Shaorune <ykmran@gmail.com>
2013-12-30 20:30:05 +0800

    added config

--- src/hk/coms/wtako/BukkitHelloWorld.java
+++ src/hk/coms/wtako/BukkitHelloWorld.java
@@ -8,54 +8,47 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BukkitHelloWorld extends JavaPlugin {
 
-	@Override
 	public void onEnable() {
-		getLogger().info("Hello bloody java world!");
+	    this.saveDefaultConfig();
+	    this.getConfig().options().copyDefaults(true);
+	    this.getLogger().info("Hello bloody java world!");
 	}
 	
 	public void onDisable() {
-		getLogger().info("Good-bye bloody chalon!");
-	}
-
-	public boolean fuck(CommandSender sender, Player target, int radius, int times) {
-		if (sender == target && sender.hasPermission("fuck.qualifiedToFuck")) {
-			for (int i = 0; i<times; i++) {
-				target.getWorld().createExplosion(target.getLocation(), radius);
-			}
-			target.sendMessage("You are awesome enough to fuck yourself and you just fucked yourself! Woohoo.");
-		} else if (sender == target) {
-			target.sendMessage("You can not fuck yourself you are not awesome enough!");
-		} else {
-			for (int i = 0; i<times; i++) {
-				target.getWorld().createExplosion(target.getLocation(), radius);
-			}
-			target.sendMessage("You have just been fucked by "+sender.getName()+" for "+Integer.toString(times)+" times!");
-			sender.sendMessage("You have just fucked "+target.getName()+" for "+Integer.toString(times)+" times!");
-		}
-		return true;
+	    this.getLogger().info("Good-bye bloody chalon!");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(cmd.getName().equalsIgnoreCase("fuck")) {
+		if (cmd.getName().equalsIgnoreCase("fuck")) {
 			try {
 				Player target = (Bukkit.getServer().getPlayer(args[0]));
+				fuck pair = new fuck(this, sender, target);
 				try {
-					return (fuck(sender, target, Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+					return (pair.execute(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
 				} catch (Exception ex1) {
 					try {
-						return (fuck(sender, target, Integer.parseInt(args[1]), 1));
+						return (pair.execute(Integer.parseInt(args[1]), 1));
 					} catch (Exception ex2) {
-						return (fuck(sender, target, 1, 1));
+						return (pair.execute(1, 1));
 					}
 				}
 			} catch (Exception ex3) {
 				if (args.length == 0) {
-					sender.sendMessage("I don't know which player you wanted to fuck");
+					sender.sendMessage(getConfig().getString("message.playerNotSpecfied"));
 				} else {
-					sender.sendMessage("The player you wanted to fuck with is offline.");
+					sender.sendMessage(getConfig().getString("message.playerNotFound"));
 					return true;
 				}
 	        } 
+		} else if (cmd.getName().equalsIgnoreCase("reloadfuck")) {
+		    this.saveDefaultConfig();
+	        this.getConfig().options().copyDefaults(true);
+		    this.reloadConfig();
+		    this.getLogger().info(getConfig().getString("message.pluginReloadedConsole"));
+		    if (sender instanceof Player) {
+		        sender.sendMessage(getConfig().getString("message.pluginReloadedPlayer"));
+		    }
+		    return true;
 		}
 		return false;
 	}
