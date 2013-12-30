fad5ade34f9217ac435f4603416dd1cc54d17d5e
Shaorune <ykmran@gmail.com>
2013-12-31 01:54:42 +0800

    hmmhmm

--- src/hk/coms/wtako/Main.java
+++ src/hk/coms/wtako/Main.java
@@ -8,16 +8,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Main extends JavaPlugin {
 
 	public void onEnable() {
-	    getCommand("fuck").setExecutor(new Command_fuck(this));
-	    getCommand("fuckreload").setExecutor(new Command_fuckreload(this));
-	        
 	    this.saveDefaultConfig();
 	    this.getConfig().options().copyDefaults(true);
 	    this.getLogger().info("Hello bloody java world!");
+	    
+	    getCommand("fuck").setExecutor(new Command_fuck(this));
+	    getCommand("fuckreload").setExecutor(new Command_fuckreload(this));
 	}
 	
 	public void onDisable() {
 	    this.getLogger().info("Good-bye bloody chalon!");
 	}
-	 
+    
 }
